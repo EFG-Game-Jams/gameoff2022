6469ae85aeed333a72c129ac586b70ec42ddeffb
SatriAli <SatriAli@users.noreply.github.com>
2022-11-03 13:59:41 +0100

    Basic user input example #5

---
+++ Assets/Scenes/Example/PlayerInput/ExamplePlayerInput.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ExamplePlayerInput : MonoBehaviour
+{
+    [SerializeField] Transform cameraTransform;
+
+    private Vector2 controlMove;
+    private float cameraPitch;
+
+    #region Input Message Handlers
+    private void OnInputMove(InputValue value) => controlMove = value.Get<Vector2>();
+    private void OnInputAim(InputValue value) => ApplyAimDelta(value.Get<Vector2>());
+    #endregion
+
+    private void ApplyAimDelta(Vector2 delta)
+    {
+        // horizontal
+        // applied to player (our own) transform
+        transform.Rotate(Vector3.up, delta.x);
+
+        // vertical
+        // applied to camera transform
+        // euler angles are generally a BadIdea™, but for this simple example it's fine
+        // it does mean we need 'cameraPitch' though, bit harder to read back the pitch from the camera transform robustly
+        cameraPitch -= delta.y;
+        cameraPitch = Mathf.Clamp(cameraPitch, -90f, 90f);
+        cameraTransform.localEulerAngles = new Vector3(cameraPitch, 0, 0);
+    }
+
+    private void Update()
+    {
+        // calculate world space velocity
+        Vector3 velocity = Vector3.zero;
+        velocity += controlMove.x * transform.right;
+        velocity += controlMove.y * transform.forward;
+
+        // calculate new position and clamp it
+        Vector3 newPosition = transform.position + velocity * Time.deltaTime;
+        newPosition.x = Mathf.Clamp(newPosition.x, 0f, 10f);
+        newPosition.y = Mathf.Clamp(newPosition.y, 0f, 10f);
+
+        // apply new position to transform
+        transform.position = newPosition;
+    }
+}
