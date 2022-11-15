98b451a060dcd47cf71960b9c0dfce6018c48e55
Saeed Ektesabi <sektesabi@bol.com>
2022-11-15 09:39:36 +0100

    improved the SO script

--- src/game/Assets/Scenes/Prototyping/Saeed/PlayerData.cs
+++ src/game/Assets/Scenes/Prototyping/Saeed/PlayerData.cs
@@ -3,10 +3,22 @@ using System.Collections;
 
 [CreateAssetMenu(fileName = "PlayerData", menuName = "ScriptableObjects/PlayerData", order = 1)]
 public class PlayerData : ScriptableObject {
-    public string objectName = "PlayerData";
+
+    // Shows the speed of player (can be the magnitude)
     public float speed;
+
+    // Max speed which player can reach. Used to scale the speed on UI
     public float maxSpeed;
-    public int shotsReady;
-    public float reloadTime;
-    public float shotsRecharchingTime;
+
+    // How many rockets player has ready
+    public int rocketReadyCount;
+
+    // Timer showing the reloading of ready rockets
+    public float rocketReloadTime;
+
+    // Timer showing the regeneration of rockets
+    public float rocketRegenTime;
+
+    // Level number
+    public int levelNumber;
 }
