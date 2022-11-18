5ae9b0a69fefad23fca92049fbf437068c272c6c
SatriAli <SatriAli@users.noreply.github.com>
2022-11-18 09:38:00 +0100

    added a title to rocket buttons with an option to display it on hover / always

--- src/game/Assets/Shared/RocketButton/RocketButton.cs
+++ src/game/Assets/Shared/RocketButton/RocketButton.cs
@@ -3,11 +3,17 @@ using UnityEngine.Events;
 
 public class RocketButton : MonoBehaviour
 {
+    [SerializeField] string title;
+    [SerializeField] bool alwaysDisplayTitle;
+    [SerializeField] bool doNotConsumeRocket;
+    [SerializeField] public UnityEvent onTrigger;
+
+    [Space]
+
     [SerializeField] Material materialIdle;
     [SerializeField] Material materialHover;
     [SerializeField] MeshRenderer mesh;
-    [SerializeField] bool doNotConsumeRocket;
-    [SerializeField] public UnityEvent onTrigger;
+    [SerializeField] TMPro.TextMeshPro text;
 
     public bool ShouldConsumeRocket => !doNotConsumeRocket;
 
@@ -22,13 +28,26 @@ public class RocketButton : MonoBehaviour
     public void OnHoverEnter()
     {
         mesh.sharedMaterial = materialHover;
+        text.gameObject.SetActive(true);
     }
     public void OnHoverExit()
     {
         mesh.sharedMaterial = materialIdle;
+        text.gameObject.SetActive(alwaysDisplayTitle);
     }
     public void OnRocketImpact()
     {
         onTrigger?.Invoke();
+    }
+
+    private void OnValidate()
+    {
+        if (text != null)
+            text.text = title;
+    }
+
+    private void Awake()
+    {
+        text.gameObject.SetActive(alwaysDisplayTitle);
     }
 }
