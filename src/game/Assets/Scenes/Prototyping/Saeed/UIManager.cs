6107d16b06c478f36872542b63d39914a7768e86
saeedek <ektesabi.saeed@gmail.com>
2022-11-18 16:24:25 +0100

    UI improvement

--- src/game/Assets/Scenes/Prototyping/Saeed/UIManager.cs
+++ src/game/Assets/Scenes/Prototyping/Saeed/UIManager.cs
@@ -58,11 +58,11 @@ public class UIManager : MonoBehaviour
         rocketLauncher.sprite = playerData.isRocketLoaded ? rocketLauncherLoaded : rocketLauncherEmtpy;
         
         // Level number
-        levelNumberHolder.SetActive(string.IsNullOrEmpty(playerData.levelNumberText));
+        levelNumberHolder.SetActive(!string.IsNullOrEmpty(playerData.levelNumberText));
         levelNumberText.text = playerData.levelNumberText;
         
         // Level timer
-        levelTimerHolder.SetActive(string.IsNullOrEmpty(playerData.levelTimerText));
+        levelTimerHolder.SetActive(!string.IsNullOrEmpty(playerData.levelTimerText));
         levelTimerText.text = playerData.levelTimerText;
 
     }
